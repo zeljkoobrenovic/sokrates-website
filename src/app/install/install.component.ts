2db7ae2bf95b3251c46286936c424e4c36a18262
zeljkoobrenovic <zeljko@incision.care>
2020-05-06 20:28:18 +0200

    dist

--- src/app/install/install.component.ts
+++ src/app/install/install.component.ts
@@ -19,4 +19,40 @@ export class InstallComponent implements OnInit {
   ngOnInit() {
   }
 
+  getBatch() {
+    return '# remove the temporary analysis folder\n' +
+      'rm -rf analysis\n' +
+      '\n' +
+      '# remove the reports folder\n' +
+      'rm -rf reports\n' +
+      '\n' +
+      '# create temporary analysis folder\n' +
+      'mkdir analysis\n' +
+      'mkdir analysis/_sokrates\n' +
+      '#create the reports folder\n' +
+      'mkdir reports\n' +
+      '\n' +
+      '# copy Sokrates cofiguration file to the temporary folder\n' +
+      'cp config.json analysis/_sokrates\n' +
+      '\n' +
+      'cd analysis\n' +
+      '\n' +
+      '# install latest version of Sokrates\n' +
+      'curl -O https://d2bb1mtyn3kglb.cloudfront.net/builds/sokrates-0.9.jar\n' +
+      '\n' +
+      '# checkout the code\n' +
+      'git clone https://github.com/zeljkoobrenovic/sokrates.git\n' +
+      'mv temp-git-dir/* .\n' +
+      'rm -rf temp-git-dir\n' +
+      '\n' +
+      '# run analysis\n' +
+      'java -jar -Xmx10g sokrates-0.9.jar generateReports -reportAll\n' +
+      '\n' +
+      '# copy generated reports to the reports folder\n' +
+      'cp -r _sokrates/reports/* ../reports\n' +
+      '\n' +
+      'cd ..\n' +
+      '# remove the temporary analysis folder\n' +
+      'rm -rf analysis\n';
+  }
 }
