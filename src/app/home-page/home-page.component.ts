18fd7516a429c8223e83663171b72f9c330d570d
zeljkoobrenovic <zeljko@incision.care>
2020-05-17 16:32:40 +0200

    dist

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home-page',
@@ -6,8 +7,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home-page.component.less']
 })
 export class HomePageComponent implements OnInit {
+  isSmallScreen = false;
 
-  constructor() {
+  constructor(private breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+      .subscribe(result => this.isSmallScreen = breakpointObserver.isMatched('(max-width: 799px)'));
   }
 
   ngOnInit() {
