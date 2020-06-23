477a7dc9c34349cddf0ff8089791d80deb89bb97
zeljkoobrenovic <zeljko@incision.care>
2020-06-23 10:25:13 +0200

    dist

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 ng build --prod
 
+cd blog-usage-src
+bash install.sh
+cd ..
 mkdir dist/website/usage
 cp -r blog-usage-src/_site/* dist/website/usage
 
+cd blog-src
+bash install.sh
+cd ..
 mkdir dist/website/concepts
 cp -r blog-src/_site/* dist/website/concepts
 
