cc0c378612a665966617188f08b5a405dcd901c2
zeljkoobrenovic <zeljko@incision.care>
2020-06-26 09:56:09 +0200

    dist

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ ng build --prod
 cd blog-src
 bash install.sh
 cd ..
-mkdir dist/website/concepts
-cp -r blog-src/_site/* dist/website/concepts
+mkdir dist/website/book
+cp -r blog-src/_site/* dist/website/book
 
 git add .
 git commit -m "dist"
