5bc0aef3af875998ae10a2ffe10b4f3d1883a34c
zobrenovic <ml!X8!S!k&7f>
2022-02-04 10:13:39 +0100

    dist

--- path.sh
+++ path.sh
@@ -1,1 +1,1 @@
-export PATH="/Users/zobrenovic/WebstormProjects/ecg-tech-explorer/node_modules/.bin:/Users/zobrenovic/.rbenv/versions/2.7.2/bin/:$PATH"
+export PATH="/Users/zobrenovic/WebstormProjects/ecg-tech-explorer/node_modules/.bin:/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin:/Users/zobrenovic/.rbenv/versions/2.7.2/bin/"
