aaa9e4a19590dd895c4128b35b293b7377207fd6
zeljkoobrenovic <zeljkoobrenovic@yahoo.com>
2022-06-09 12:48:12 +0200

    dist

--- path-ng.sh
+++
@@ -1,1 +0,0 @@
-export PATH=/Users/zobrenovic/WebstormProjects/ecg-tech-explorer/node_modules/.bin:/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin
--- path.sh
+++
@@ -1,1 +0,0 @@
-export PATH="/Users/zobrenovic/WebstormProjects/ecg-tech-explorer/node_modules/.bin:/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin:/Users/zobrenovic/.rbenv/versions/2.7.2/bin/"
